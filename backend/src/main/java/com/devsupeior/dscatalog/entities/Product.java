83fcb72a26a7165f1725d1ecef4f67c9e9e2dc25
Felipe Gonzales <felipe.gonzales37@gmail.com>
2021-01-20 18:54:08 -0300

    Product repository, service, resource, GET ok

--- backend/src/main/java/com/devsupeior/dscatalog/entities/Product.java
+++ backend/src/main/java/com/devsupeior/dscatalog/entities/Product.java
@@ -43,13 +43,13 @@ public class Product implements Serializable {
 		
 	}
 
-	public Product(Long id, String name, String description, Double price, String imgUri, Instant date) {
+	public Product(Long id, String name, String description, Double price, String imgUrl, Instant date) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.description = description;
 		this.price = price;
-		this.imgUrl = imgUri;
+		this.imgUrl = imgUrl;
 		this.date = date;
 	}
 
@@ -85,12 +85,12 @@ public class Product implements Serializable {
 		this.price = price;
 	}
 
-	public String getImgUri() {
+	public String getImgUrl() {
 		return imgUrl;
 	}
 
-	public void setImgUri(String imgUri) {
-		this.imgUrl = imgUri;
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
 	}
 
 	public Instant getDate() {
