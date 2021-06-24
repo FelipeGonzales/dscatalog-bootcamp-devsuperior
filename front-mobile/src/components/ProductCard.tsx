d0e1e0d0e80106d9339177ac7ffe9795bd537425
Felipe Gonzales <felipe.gonzales37@gmail.com>
2021-06-24 16:15:38 -0300

    fix front-mobile initial files and product details

---
+++ front-mobile/src/components/ProductCard.tsx
@@ -0,0 +1,52 @@
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { View, Text, ImageSourcePropType, TouchableOpacity, Image } from 'react-native';
+import { text, theme } from '../styles';
+
+interface ProductProps {
+    id: Number;
+    imgUrl: ImageSourcePropType;
+    name: String;
+    price: Number;
+    role?:String
+}
+
+
+const ProductCard: React.FC<ProductProps> = ({ id, imgUrl, name, price, role }) => {
+    const navigation = useNavigation();
+    return (
+        <TouchableOpacity style={theme.productCard} onPress={() =>
+            navigation.navigate("ProductDetails", {id})
+        }>
+            <Image source={imgUrl} style={theme.productImg}/>
+            <View style={theme.productDescription}>
+                <Text style={text.productName}>
+                    {name}
+                </Text>
+                <View style={theme.priceContainer}>
+                    <Text style={text.currency}>
+                        R$
+                </Text>
+                    <Text style={text.productPrice}>
+                        {price}
+                    </Text>
+                </View>
+                {
+                    role === "admin" && (
+                        <View style={theme.buttonContainer}>
+                            <TouchableOpacity style={theme.deleteBtn}>
+                                <Text style={text.deleteTxt}>Excluir</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={theme.editBtn}>
+                                <Text style={text.editTxt}>Editar</Text>
+                            </TouchableOpacity>
+                            </View>
+                    )
+                }
+            </View>
+        </TouchableOpacity>
+    )
+
+}
+
+export default ProductCard; 
